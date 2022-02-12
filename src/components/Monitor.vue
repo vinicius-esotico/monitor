5d1966cedd5d8d1196b1c60e3344bf1870f1145c
vinicius-esotico <49448844+vinicius-esotico@users.noreply.github.com>
2022-02-12 13:28:59 -0300

    Erro 404 ao solicitar API

---
+++ src/components/Monitor.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="monitor">
+    <h1>{{ title }}</h1>
+    <p>API: {{ apiKey }}</p>
+    <p>Data: {{ data }}</p>
+    <p>Error: {{ error }}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  props: {
+    apiKey: {type: String},
+    title: {type: String},
+  },data: function () {
+    return {
+      data: null,
+      error: '',
+      config: {
+        method: 'POST',
+        url: 'https://api.uptimerobot.com/v2/getMonitors',
+        headers: { 
+          'cache-control': 'no-cache',
+          'content-type': 'application/x-www-form-urlencoded' 
+          },
+        form: { 
+          api_key: this.apiKey, format: 'json', logs: '1' 
+        }
+      }
+    }
+},
+  mounted() {
+    axios.get(this.config)
+          .then(response => {
+            this.data = response
+          }).catch(error => {
+            this.error = error
+          })
+  }
+}
+</script>
+
+<style>
+h1 {
+  font-size: 2em;
+  padding: 20px;
+}
+
+.monitor {
+  width: 100%;
+  height: fit-content;
+  background-color: #ccc;
+
+}
+
+</style>
---
+++ src/main/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <Monitor 
+    apiKey="u1615701-5e5dece8faf97842b13d211f"
+    title="Monitor Esotico.com.br"
+    />
+  </div>
+</template>
+
+<script>
+
+import Monitor from '../components/Monitor'
+
+export default {
+  components: {
+    Monitor
+  }
+}
+</script>
+
+<style>
+.main {
+  display: flex;
+  justify-content: center;
+}
+</style>
