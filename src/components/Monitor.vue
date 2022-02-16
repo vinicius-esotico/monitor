8c40ee144a20cc4b86677172ea180888f6268ce3
vinicius-esotico <49448844+vinicius-esotico@users.noreply.github.com>
2022-02-16 07:40:36 -0300

    Implementação V-For

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="monitor">
     <h1>{{ title }}</h1>
-    <p>API: {{ apiKey }}</p>
-    <p>Data: {{ data }}</p>
-    <p>Error: {{ error }}</p>
+      <div v-for="monitor in sites" class="monitor-row"> 
+        <div class="name">
+          <span>{{ monitor.friendly_name }}</span>
+        </div>
+        <div class="url">
+          <span>{{ sites[0].url }}</span>
+        </div>
+        <div class="status">
+          <span>{{ status(sites[0].status) }}</span>
+        </div>
+      </div>
+    <p>{{ error }}</p>
   </div>
 </template>
 
@@ -17,33 +26,39 @@ export default {
     title: {type: String},
   },data: function () {
     return {
-      data: null,
+      status: null,
+      sites: [],
       error: '',
       resp: '',
-      config: {
-        method: 'GET',
-        withCredentials: false,
-        baseURL: 'http://api.uptimerobot.com/v2',
         headers: { 
           'cache-control': 'no-cache',
-          'Access-Control-Allow-Origin': '*',
           'content-type': 'application/x-www-form-urlencoded' 
-          },
-        form: { 
-          api_key: this.apiKey, format: 'json', logs: '1' 
         }
-      }
     }
-},
-  mounted() {
-      axios.get('/getMonitors', this.config)
-          .then(resp => {
-            this.data = resp
-          }).catch(error => {
-            this.error = error
-          })
+},mounted() {
+    axios.post(`https://api.uptimerobot.com/v2/getMonitors?api_key=${this.apiKey}`, this.headers)
+        .then(resp => {
+          [...this.sites] = resp.data.monitors
+        }).catch(error => {
+          this.error = error
+        })
 
-    }
+        this.status = function status(i) {
+          switch(i) {
+            case 0:
+              return 'paused'
+            case 1:
+              return 'not checked yet'
+            case 2:
+              return 'up'
+            case 8:
+              return 'seems down'
+            case 9:
+              return 'down'
+          }
+        }
+      // 5 minutos em milisegundos = 30000
+  }
 }
 </script>
 
@@ -56,6 +71,13 @@ h1 {
 .monitor {
   width: 100%;
   height: fit-content;
+}
+
+.monitor-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 10px;
 }
 
 </style>
